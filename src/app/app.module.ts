7dd52f650bee7d0b771bf82bfd9e575e23ab7e96
Fabingr <fabian.eisinger@gmail.com>
2022-12-14 14:19:10 +0100

    add style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RankingComponent } from './ranking/ranking.component';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ScrollingModule} from "@angular/cdk/scrolling";
+import {MatListModule} from "@angular/material/list";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { RankingComponent } from './ranking/ranking.component';
     RankingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    ScrollingModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
